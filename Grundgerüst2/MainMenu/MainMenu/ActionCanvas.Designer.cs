c1a72373aefe7bede6e9c0765ac5b054014c7011
stuff <tommy.pototzki@netzkern.de>
2016-01-26 21:42:28 +0100

    Animationsfunkion / update Gameloop Methode /   Frameskip Methode

---
+++ Grundgerüst2/MainMenu/MainMenu/ActionCanvas.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace MainMenu
+{
+    partial class ActionCanvas
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(98, 187);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ActionCanvas
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(998, 567);
+            this.Controls.Add(this.btnStart);
+            this.DoubleBuffered = true;
+            this.Name = "ActionCanvas";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ActionCanvas_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
--- Grundgerüst2/MainMenu/MainMenu/ActionCanvas.cs
+++ Grundgerüst2/MainMenu/MainMenu/ActionCanvas.cs
@@ -17,19 +17,14 @@ namespace MainMenu
 
         // Inizialisiere Bild (komisch wieso ich das hier machen muss und nicht in der eigentlichen funktion dafür...)
         Bitmap LinkWalkAnim = new Bitmap("E:\\Das-Labyrinth\\Grundgerüst\\MainMenu\\MainMenu\\src\\link_move_anim.png");
-        static int xFigure = 50;
-        static int yFigure = 10;
-        static byte figureFrame = 0;
-
         Boolean isDebug = true;
-        Boolean isStart = true;
+        Boolean isStart = false;
 
         public ActionCanvas()
         {
             //SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
             //UpdateStyles();
             InitializeComponent();
-            btnStart.Visible = false;
             try
             {
                 // Inizialisiere Bild
@@ -41,7 +36,7 @@ namespace MainMenu
             // Erstellt Form unabhängigen Timer
 
             System.Timers.Timer a = new System.Timers.Timer();      // Neues Timer-Objekt
-            a.Interval = 1000 / 30;                                        // Intervall der Ausführung der A_Elapsed-Methode
+            a.Interval = 10;                                        // Intervall der Ausführung der A_Elapsed-Methode
             a.Elapsed += A_Elapsed;
             a.Enabled = true;
         }
@@ -62,27 +57,13 @@ namespace MainMenu
             }
         }
 
-        DateTime lastUpdate = DateTime.MinValue; // Letzer Zeitpunkt des Updates von Game-Logik
-        TimeSpan updateInterval = new TimeSpan(0, 0, 0, 0, 66); // Wie oft soll die Game-Logik aktualisiert werden (50ms)
-       
         // Loop-Methode die alle x ms aufgerufen wird (siehe Intervall System-Timers)
         private void A_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
             if (isStart)
             {
             }
-
-            // Wenn Jetzt - letzter Zeitpunkt des Game-Logik-Updates grösser als die 50ms Update-Intervall sind, dann erneut Updaten
-            if (DateTime.Now - lastUpdate > updateInterval)
-            {
-                // Hier wird nun die Logik des Spiels vorwärts getrieben, bzw. das vom Frame erhöhen
-                UpdateGameLogic();
-                lastUpdate = DateTime.Now; // Zuletzt updated = Jetzt
-            }
-
-            // Immer Zeichnen
             Repaint(); // Ruft Methode zum übermalen der aktuellen Leinwand auf
-            // Colli Logik hier rein weil keine framesperre
         }
 
         // Refresh Methode zur Bereinigung der Leinwand
@@ -114,22 +95,18 @@ namespace MainMenu
             if (up)
             {
                 yLinkWalkAnim -= speedObject;
-                figureFrame = getFrame(figureFrame, (byte)14, (byte)20,(byte)4);
             }
             if (down)
             {
                 yLinkWalkAnim += speedObject;
-                figureFrame = getFrame(figureFrame, (byte)21, (byte)27, (byte)4);
             }
             if (left)
             {
                 xLinkWalkAnim -= speedObject;
-                figureFrame = getFrame(figureFrame, (byte)7, (byte)13, (byte)4);
             }
             if (right)
             {
                 xLinkWalkAnim += speedObject;
-                figureFrame = getFrame(figureFrame, (byte)0, (byte)6, (byte)4);
             }
         }
 
@@ -141,56 +118,31 @@ namespace MainMenu
             i++;
             switch (i)
             {
-                case 40: down = false; right = true;
+                case 150: down = false; right = true;
                     break;
-                case 80: right = false; up = true;
+                case 300: right = false; up = true;
                     break;
-                case 120: up = false; left = true;
+                case 450: up = false; left = true;
                     break;
-                case 160: left = false; down = true; i = 0;
+                case 600: left = false; down = true; i = 0;
                     break;
                 default:
                     break;
             }
         }
 
-        // plotFrame Methode initialisiert und definiert das Bild und die Bereiche
-        public static void plotFrame (
-            Bitmap bitmap, // Bildobjekt laden
-            int width, // Bildlänge eines Frames
-            int height, // Bildhöhe eines Frames
-            int frame, // Frame zur kontrolle der Geschwindigkeit
-            int x, //Position des Bild x
-            int y, // Position des Bild y
-            PaintEventArgs e
-            ) {
-            Rectangle clipRect = new Rectangle(x, y, width, height); // Clippingbereich definieren
-            e.Graphics.SetClip(clipRect); // Clippen
-            e.Graphics.DrawImage(bitmap, x-frame*width, y); // Zieht den Animationsstreifen per Frame (x-Pos des Bildes - Frame * Bildlänge) 
-        }
-
-        // Eine Methode zur Kontrolle der Framerate um das Bild langsamer zu machen (klappt nicht so ganz :P)
-        static byte timerFrame = 0;
-        public static byte getFrame(byte frame, byte min, byte max, byte step) {
-            if (frame < min || frame > max) frame = min;
-            if (step < 1) step = 1;
-            if (timerFrame % step == 0)
-            {
-                frame++;
-                if (frame > max) frame = (byte) min;
-            }
-            return frame;
-        }
-
-        // Spiel-Logik die etwas verlangsamt ausgeführt wird gegenüber dem Zeichnen (kontrolliert)
-        void UpdateGameLogic()
+        // Versuche eigene Methode zu bauen für autom. clipping die auf alle zukünftigen Objekte angewendet werden können
+        public static void plotFrame(Image image, int width, int height, int frame, int x, int y, PaintEventArgs e)
         {
-            moveObject();
+            Rectangle clipRect = new Rectangle(0, 0, 25, 30);
+            e.Graphics.SetClip(clipRect);
         }
 
         // Leinwand Methode die alles grafische darstellt
         private void ActionCanvas_Paint(object sender, PaintEventArgs e)
         {
+            if (isStart)
+            {
                 //e.Graphics.DrawRectangle(Pens.Black, 30, 30, 0+i, 0+i); // lusdisch... aber useless xD
                 //e.Graphics.DrawImage(LinkWalkAnim, new Point(xLinkWalkAnim, yLinkWalkAnim));
 
@@ -200,26 +152,30 @@ namespace MainMenu
                 Heisst: Bild ist 700 x30. Es handelt sich bei der Animation um 28 Einzerlbilder. Bedeutet 700 / 28 = 25. Bedeutet wir clippen
                 ein Feld von 25 x 30.
                 */
-            //Rectangle clipRect = new Rectangle(0, 0, 25, 30);
-            //e.Graphics.SetClip(clipRect);
-            //e.Graphics.DrawImage(LinkWalkAnim, 0, 0, LinkWalkAnim.Width, LinkWalkAnim.Height);
 
-            /* ---Image Loading mit Transparenz)
-            Hier erstellen wir ein Image-Objekt. Davon holen wir die Hintergrundfarbe (R = 255, G = 0, B = 255) und sagen,
-            dass diese Transparent sein soll.
-            Unmittelbar danach, laden wir das das gleiche Objekt nochmal nur das diesesmal die Hintergrundfarbe Transparent ist.
-            Ich denke mal, dass wir das in einer Toolbox später auslagern können und wenn wir uns auf eine allgemeine Transparentfarbe einigen,
-            wir nicht stetig prüfen müssen um welche Hintergrundfarbe es sich handelt beim aktuellen Objekt.
-            */
+                Rectangle clipRect = new Rectangle(0, 0, 25, 30);
+                //Rectangle clipRectEnd = new Rectangle(0, 0, w, h);
+                e.Graphics.SetClip(clipRect);
+                e.Graphics.DrawImage(LinkWalkAnim, 0, 0, LinkWalkAnim.Width, LinkWalkAnim.Height);
 
-            //e.Graphics.DrawImage(LinkWalkAnim, 0, 0, LinkWalkAnim.Width, LinkWalkAnim.Height);
-            // plotMethode zeichnen
-            plotFrame(LinkWalkAnim, 25, 30, figureFrame, xFigure, yFigure, e);
-            Color backColor = LinkWalkAnim.GetPixel(1, 1);
-            LinkWalkAnim.MakeTransparent(backColor);
+                /* ---Image Loading mit Transparenz)
+                Hier erstellen wir ein Image-Objekt. Davon holen wir die Hintergrundfarbe (R = 255, G = 0, B = 255) und sagen,
+                dass diese Transparent sein soll.
+                Unmittelbar danach, laden wir das das gleiche Objekt nochmal nur das diesesmal die Hintergrundfarbe Transparent ist.
+                Ich denke mal, dass wir das in einer Toolbox später auslagern können und wenn wir uns auf eine allgemeine Transparentfarbe einigen,
+                wir nicht stetig prüfen müssen um welche Hintergrundfarbe es sich handelt beim aktuellen Objekt.
+                */
 
-            // Moving around some stuff
-            moveRect();
+                e.Graphics.DrawImage(LinkWalkAnim, 0, 0, LinkWalkAnim.Width, LinkWalkAnim.Height);
+                Color backColor = LinkWalkAnim.GetPixel(1, 1);
+                LinkWalkAnim.MakeTransparent(backColor);
+                //e.Graphics.DrawImage(LinkWalkAnim, 0, 0, LinkWalkAnim.Width, LinkWalkAnim.Height);
+
+
+                // Moving around some stuff
+                moveObject();
+                moveRect();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
